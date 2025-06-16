da16be405aaced6be7681a98de14555bb4139203
AaronDeluna <aaron_deluna@mail.ru>
2025-06-16 08:18:52 +0300

    Update add Builder

--- src/main/java/org/slatto/notification/dto/appointment/AppointmentBookingClientDto.java
+++ src/main/java/org/slatto/notification/dto/appointment/AppointmentBookingClientDto.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import org.slatto.notification.dto.BaseNotificationDto;
 
+@Builder
 @AllArgsConstructor
 public class AppointmentBookingClientDto extends BaseNotificationDto {
 }
--- src/main/java/org/slatto/notification/dto/auth/RegistrationNotificationDto.java
+++ src/main/java/org/slatto/notification/dto/auth/RegistrationNotificationDto.java
@@ -1,8 +1,10 @@
 package org.slatto.notification.dto.auth;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import org.slatto.notification.dto.BaseNotificationDto;
 
+@Builder
 @AllArgsConstructor
 public class RegistrationNotificationDto extends BaseNotificationDto {
     private String username;
