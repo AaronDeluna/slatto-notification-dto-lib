3f13873f78abc70b66bb63c9b823a4f40465f121
AaronDeluna <aaron_deluna@mail.ru>
2025-06-16 09:44:47 +0300

    Update appoimentDto

--- src/main/java/org/slatto/notification/dto/appointment/AppointmentBookingNotificationDto.java
+++ src/main/java/org/slatto/notification/dto/appointment/AppointmentBookingNotificationDto.java
@@ -7,10 +7,18 @@ import lombok.ToString;
 import lombok.experimental.SuperBuilder;
 import org.slatto.notification.dto.BaseNotificationDto;
 
+import java.time.LocalDateTime;
+
 @Data
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
 @SuperBuilder
 @AllArgsConstructor
-public class AppointmentBookingClientDto extends BaseNotificationDto {
+public class AppointmentBookingNotificationDto extends BaseNotificationDto {
+    private String offeringName;
+    private String clientFirstName;
+    private String clientLastName;
+    private LocalDateTime date;
+    private String masterFirstName;
+    private String masterLastName;
 }
